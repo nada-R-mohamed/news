db5c190b545f79e3ff177e4d6937bae443cca140
Nada <apple@apples-MacBook-Pro.local>
2025-10-05 13:12:23 +0300

    chore: install laravel ui package

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('frontend.index');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
