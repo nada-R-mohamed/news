211982fb8d9ca2b5b0e5c4c722180a549bf50431
Nada <apple@apples-MacBook-Pro.local>
2025-10-05 15:59:48 +0300

    fix: correct image path generation in ImageFactory

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Image>
@@ -18,8 +19,8 @@ class ImageFactory extends Factory
     public function definition(): array
     {
         return [
-            'path' => fake()->imageUrl(),
-            
+            'path' => 'https://picsum.photos/seed/' . (string) Str::uuid() . '/640/480',
+
         ];
     }
 }
